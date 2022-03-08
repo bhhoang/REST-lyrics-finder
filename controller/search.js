4628ec12895793cb9a9655b40647da5b6515811c
bhhoang <canhchimcuajin@gmail.com>
2022-03-08 11:03:17 +0700

    Optimize Code

--- controller/search.js
+++ controller/search.js
@@ -1,34 +1,47 @@
 const cheerio = require('cheerio');
 const request = require('request');
-var songobj = {};
 
-const search = function (query){
+const search = function (query, callback) {
+  let songobj = new Object();
+  try {
     request("https://www.nhaccuatui.com/tim-kiem?q=" + query, (error, response, html) => {
-        if(!error && response.statusCode == 200) {
-          const $ = cheerio.load(html);
-      
-          const url = $($('.sn_search_single_song')['0']).find('a').attr('href');
-          const songName = $($('.sn_search_single_song')['0']).find('.title_song a').attr('title');
-          const singer = $($('.sn_search_single_song')['0']).find('.singer_song a').text();
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const url = $($('.sn_search_single_song')['0']).find('a').attr('href');
+        const songName = $($('.sn_search_single_song')['0']).find('.title_song a').attr('title');
+        const singer = $($('.sn_search_single_song')['0']).find('.singer_song a').text();
+        if (url) {
           return request(url, (error, response, html) => {
-                if(!error && response.statusCode == 200) {
-                    const ra = cheerio.load(html);
-                    let lyrics = ra('#divLyric').text();
-                    let lines = lyrics.split('\n');
-                    lines = lines.filter(function(entry) { return entry.trim() != '' });
-                    (lines[0].includes("Bài hát")) ? lines.shift() : null;
-                    lyrics = lines.join('\n');
-                    songobj.name = songName;
-                    songobj.singer = singer;
-                    songobj.lyrics = lyrics.trim();
-                }
+            if (!error && response.statusCode == 200) {
+              const ra = cheerio.load(html);
+              let lyrics = ra('#divLyric').text();
+              let lines = lyrics.split('\n');
+              lines = lines.filter(function (entry) { return entry.trim() != '' });
+              (lines[0].includes("Bài hát")) ? lines.shift() : null;
+              lyrics = lines.join('\n');
+              songobj.name = songName;
+              songobj.singer = singer;
+              songobj["api author"] = "Nanika";
+              songobj.hoster = "";
+              songobj.lyrics = lyrics.trim();
+              callback(songobj);
+            }
           })
         }
         else {
-          return;
+          songobj.error = "Không tìm thấy bài hát hoặc API bị hỏng";
+          songobj["api author"] = "Nanika";
+          songobj.hoster = "";
+          return callback(songobj);
         }
       }
-    )
+    })
+  }
+  catch (error) {
+    songobj.error = "Lỗi" + error;
+    songobj.author = "Nanika";
+    songobj.hoster = "";
+    return songobj
+  }
 }
 module.exports = search;
-module.exports.songobj = songobj;
--- router/route.js
+++ router/route.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const search = require('../controller/search');
-const songobj = require('../controller/search').songobj;
 
 router.get('/',(req,res)=>{
     res.writeHead(200, {'Content-Type': 'application/json'});
     const q = req.query.q;
     const que = encodeURIComponent(q);
-    search(que);
-    setTimeout(()=>res.end(JSON.stringify(songobj, null, 4)),5000);
+    search(que, writeEnd);
+    function writeEnd(songobj){
+        res.end(JSON.stringify(songobj, null, 4));
+    }
 })
 
 module.exports = router;
