892ba115100ade9ebe28a8fc8fc5a97a6a2bc114
bhhoang <canhchimcuajin@gmail.com>
2022-03-13 16:48:41 +0700

    Switch to axios

--- controller/search.js
+++ controller/search.js
@@ -1,52 +1,51 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const axios = require('axios');
 
 const search = function (query, callback) {
   let songobj = new Object();
-  try {
-    request("https://www.nhaccuatui.com/tim-kiem?q=" + query, (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        let url = $($('.sn_search_single_song')['0']).find('a').attr('href');
-        let songName = $($('.sn_search_single_song')['0']).find('.title_song a').attr('title');
-        let singer = $($('.sn_search_single_song')['0']).find('.singer_song a').text();
-        if ($('.sn_box_search_suggest')){
-            songName = $($('.sn_box_search_suggest')[0]).find('.sn_name_album_search a').attr('title');
-            singer =  $($('.sn_box_search_suggest')[0]).find('.sn_list_singer_search a').text();
-            url = $($('.sn_box_search_suggest')[0]).find('.sn_thumb a').attr('href');
-        }
-        if (url) {
-          return request(url, (error, response, html) => {
-            if (!error && response.statusCode == 200) {
-              const ra = cheerio.load(html);
-              let lyrics = ra('#divLyric').text();
-              let lines = lyrics.split('\n');
-              lines = lines.filter(function (entry) { return entry.trim() != '' });
-              (lines[0].includes("Bài hát")) ? lines.shift() : null;
-              lyrics = lines.join('\n');
-              songobj.name = songName;
-              songobj.singer = singer;
-              songobj["api author"] = "Nanika";
-              songobj.hoster = "";
-              songobj.lyrics = lyrics.trim();
-              return callback(songobj);
-            }
-          })
-        }
-        else {
-          songobj.error = "Không tìm thấy bài hát hoặc API bị hỏng";
-          songobj["api author"] = "Nanika";
-          songobj.hoster = "";
-          return callback(songobj);
-        }
-      }
-    })
-  }
-  catch (error) {
-    songobj.error = "Lỗi" + error;
-    songobj.author = "Nanika";
+  axios.get('https://www.nhaccuatui.com/tim-kiem?q=' + query).then(res => {
+    const $ = cheerio.load(res.data);
+    let url = $($('.sn_search_single_song')['0']).find('a').attr('href');
+    let songName = $($('.sn_search_single_song')['0']).find('.title_song a').attr('title');
+    let singer = $($('.sn_search_single_song')['0']).find('.singer_song a').text();
+    if (($('.sn_box_search_suggest')).length != 0) {
+      songName = $($('.sn_box_search_suggest')[0]).find('.sn_name_album_search a').attr('title');
+      singer = $($('.sn_box_search_suggest')[0]).find('.sn_list_singer_search a').text();
+      url = $($('.sn_box_search_suggest')[0]).find('.sn_thumb a').attr('href');
+    }
+    if (url) {
+      axios.get(url).then(res => {
+        const ra = cheerio.load(res.data);
+        let lyrics = ra('#divLyric').text();
+        let lines = lyrics.split('\n');
+        lines = lines.filter(function (entry) { return entry.trim() != '' });
+        (lines[0].includes("Bài hát")) ? lines.shift() : null;
+        lyrics = lines.join('\n');
+        songobj.name = songName;
+        songobj.singer = singer;
+        songobj["api author"] = "Nanika";
+        songobj.hoster = "";
+        songobj.lyrics = lyrics.trim();
+        return callback(songobj);
+      }).catch(err => {
+        songobj.error = "Lỗi " + err;
+        songobj["api author"] = "Nanika";
+        songobj.hoster = "";
+        return callback(songobj);
+      })
+    }
+    else {
+      songobj.error = "Không tìm thấy bài hát";
+      songobj["api author"] = "Nanika";
+      songobj.hoster = "";
+      return callback(songobj);
+    }
+  }).catch(err => {
+    songobj.error = "Lỗi " + err;
+    songobj["api author"] = "Nanika";
     songobj.hoster = "";
-    return songobj
-  }
+    return callback(songobj);
+  })
 }
+
 module.exports = search;
